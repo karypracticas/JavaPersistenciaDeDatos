d3298a7a87ea2dafdfbb5388af00d1734b8c5b46
practicaskary2 <practicasgithub2@gmail.com>
2020-08-30 10:03:02 -0500

    Crear conexion exitosa a base de datos

--- src/main/java/com/mycompany/mensajes_app/inicio.java
+++ src/main/java/com/mycompany/mensajes_app/inicio.java
@@ -6,7 +6,13 @@ package com.mycompany.mensajes_app;
  */
 public class inicio {
     public static void main(String[] args) {
-        
+       Conexion obj = new Conexion();
+       
+       try {
+       obj.get_connection();
+       } catch(Exception e) {
+           System.out.println(e);
+       }
     }
-    
+   
 }
