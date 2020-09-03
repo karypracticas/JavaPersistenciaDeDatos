8d03bf396d42cf28272a7bb6a41d584bdc743435
practicaskary2 <practicasgithub2@gmail.com>
2020-09-02 21:38:27 -0500

    CRUD:Lectura de datos de la Bd

--- src/main/java/com/mycompany/mensajes_app/MensajesDAO.java
+++ src/main/java/com/mycompany/mensajes_app/MensajesDAO.java
@@ -2,6 +2,7 @@ package com.mycompany.mensajes_app;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 
 /**
@@ -32,7 +33,27 @@ public class MensajesDAO {
     }
     
     public static void leerMensajesDB(){
-        
+         Conexion db_connect = new Conexion();
+         
+         PreparedStatement ps = null;
+         ResultSet rs = null;
+         
+         try (Connection conexion = db_connect.get_connection()) {
+           String query = "SELECT * FROM mensajes";
+           ps = conexion.prepareStatement(query);
+           rs = ps.executeQuery();
+           
+           while(rs.next()) {
+               System.out.println("ID: " + rs.getInt("id_mensaje"));
+               System.out.println("Mensaje: " + rs.getString("mensaje"));
+               System.out.println("Autor: " + rs.getString("autor_mensaje"));
+               System.out.println("Fecha: " + rs.getDate("fecha_mensaje"));  
+               System.out.println("**********************");
+           }   
+         }catch(SQLException e) {
+             System.out.println("No se pudieron obtener los datos");
+           System.out.println(e);
+        }      
     }
     
     public static void borrarMensajesDB(int id_mensaje) {
--- src/main/java/com/mycompany/mensajes_app/MensajesService.java
+++ src/main/java/com/mycompany/mensajes_app/MensajesService.java
@@ -23,6 +23,7 @@ public class MensajesService {
         
     }
     public static void listarMensaje() {
+        MensajesDAO.leerMensajesDB();
         
     }
     public static void borrarMensaje() {
